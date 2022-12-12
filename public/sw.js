2db4bdae64c249e5c49d9a7d1012a2ec0fb976e9
Cécile Hefti <cecile@interactivethings.com>
2022-12-12 14:52:00 +0100

    feat: Add example push notification

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,13 @@
 importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js'
-);
+  );
 
-// This is your Service Worker, you can put any of your custom Service Worker
-// code in this file, above the `precacheAndRoute` line.
+  self.addEventListener('push', () => {
+    event.waitUntil(
+      registration.showNotification("Hello!", {
+        body: "This is a push notification!",
+      })
+    );
+  });
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
